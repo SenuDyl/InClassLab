e9450bd8143a1dc895d58e41f39ed5e410d6c55a
iamsenuri <senurimallikarachchi@gmail.com>
2023-03-30 14:42:24 +0530

    Final iterative quicksort

--- QuickSortIterative.cpp
+++ QuickSortIterative.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
+
 #include <chrono>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <stdio.h>
+#include <ostream>
 using namespace std;
 using namespace std::chrono;
-
 //Return the location of the pivot
 int partition(int* array,int start,int end){
     int pivot=array[end];
@@ -26,28 +28,46 @@ int partition(int* array,int start,int end){
     array[end]=temp1;
     return i;
 }
-//Sort using divide and conquer approach
-int* quickSort(int* array,int start,int end){
-    if(end<=start){return array;}//base case
-    int pivot=partition(array,start,end);
-    quickSort(array,start,pivot-1);
-    quickSort(array,pivot+1,end);
-    return array;
+
+void quickSortIterative(int* array, int start, int end)
+{
+    int stack[end - start + 1];
+    int top = -1;
+    stack[++top] = start;
+    stack[++top] = end;
+    while (top >= 0) {
+        end = stack[top--];
+        start = stack[top--];
+        int pivot = partition(array, start, end);
+        if (pivot + 1 < end) {
+            stack[++top] = pivot + 1;
+            stack[++top] = end;
+        }
+        if (pivot - 1 > start) {
+            stack[++top] = start;
+            stack[++top] = pivot - 1;
+        }
+    }
 }
+  
 
 
-int main(){
-    auto start = high_resolution_clock::now();
+
+int main()
+{
+	auto start = high_resolution_clock::now();
     int n;
     cout<<"Total elements in the array: ";
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++)
         arr[i]=rand()%50;
-    int *newArray=quickSort(arr,0,n-1);
+    quickSortIterative(arr, 0, n - 1);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "time taken is "
        << (duration.count())*0.000001<< " seconds" << endl;
     return 0;
+	
+	return 0;
 }
